7addecf8d0d3856b7bed48b4d3c01a2fd45684f0
Eduardo Gil <bushiiko@gmail.com>
2017-02-06 11:37:30 -0600

    crud en clients

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,11 +7,30 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Response;
+
 use App\User;
 use Validator;
 
+use Vinkla\Pusher\Facades\Pusher;
+use App\Jobs\UpdateRoute;
+use App\Jobs\GenerateRoute;
+
+use App\Bounds;
+
+use App\Route;
+use App\RouteStep;
+
 class ClientController extends Controller
 {
+    private $Bounds;
+
+
+    public function __construct() {
+        $this->Bounds = new Bounds();
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -32,7 +51,48 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try
+        {
+            $rules = array(
+                'name' => 'required'
+            );
+
+            $validator = Validator::make(Input::all(), $rules);
+
+            if($validator->fails()) 
+            {
+                return response()->json($validator->messages(), 
+                    Response::HTTP_UNPROCESSABLE_ENTITY
+                );
+            }
+
+            $client = new User;
+            $client->name = Input::get('name');
+            $client->role = 'CLIENT';
+
+            // random initial coords
+            $client->lat = $this->Bounds->getRandomLat();
+            $client->lng = $this->Bounds->getRandomLng();
+
+            $client->save();
+
+            // TODO: como valido si se guardo?
+
+            Pusher::trigger('tako-channel', 'new-client', ['client' => $client]);
+
+            $job = (new GenerateRoute($client->id));
+            dispatch($job);
+
+
+            return response()->json($client);
+        }
+        catch(\Exception $e) 
+        {
+            return response()->json(
+                ['type' => 'error', 'message' => $e->getMessage()], 
+                Response::HTTP_BAD_REQUEST
+            );
+        }
     }
 
     /**
@@ -43,7 +103,14 @@ class ClientController extends Controller
      */
     public function show($id)
     {
-        //
+        $client = User::where('role', 'CLIENT')->find($id);
+
+        if(is_null($client))
+        {
+            return response('Client Not Found', Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($client);
     }
 
     /**
